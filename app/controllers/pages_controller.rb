153c4b1d3c67ebd105783842eba803af4491b8e9
Marika Pinori <marika.pinori@studenti.polito.it>
2014-06-27 21:26:43 +0200

    Base - test

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def faq
+  end
+
+  def signup
+  end
+
+  def signin
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flix::Application.config.secret_token = '7470b3a1a064abc97469976a1c33d682376b3bf953ac3eca79cae83917920321ad250fb84f2a9054dd0d8e62098ce7143ccbcdb5f54f24c7030d232b76abe3a1'
