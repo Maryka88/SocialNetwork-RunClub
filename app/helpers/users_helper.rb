db1401045ac6b9a84530e62afa9c05377b8ef93b
Marika Pinori <marika.pinori@studenti.polito.it>
2014-06-28 19:53:08 +0200

    gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+  #img che segue nome di pag in pag
+  def gravatar_for(user)
+    # codifica hash md5 email user
+    gravatar_id = Digest::MD5::hexdigest(user.email)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
