405541ca606de15d400b5be953c64ceb742a52de
Marika Pinori <marika.pinori@studenti.polito.it>
2014-06-28 20:10:56 +0200

    Form creazione utente + add user + stile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,10 @@ class UsersController < ApplicationController
   def new
     #creo utente base per testing - poi da rimuovere
     #@user = User.create(name: 'Marika', email: 'marika.pinori@polito.it', password: 'test0123', password_confirmation: 'test0123')
-    @user = User.create(name: 'Luigi', email: 'luigi.derussis@polito.it', password: 'prova123', password_confirmation: 'prova123')
+    #@user = User.create(name: 'Luigi', email: 'luigi.derussis@polito.it', password: 'prova123', password_confirmation: 'prova123')
+
+    #inizializzo la variabile da utilizzare nel form di registrazione
+    @user = User.new
   end
 
 end
