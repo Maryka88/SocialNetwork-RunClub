08d3226fb1956226b569ffda46708bd202c10dfe
Marika Pinori <marika.pinori@studenti.polito.it>
2014-06-28 17:11:55 +0200

    utente base + azione e pagina show + mod css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+  def show
+    #prendo utente dall'id :id
+    @user = User.find(params[:id])
+  end
+
+  def new
+    #creo utente base per testing - poi da rimuovere
+    @user = User.create(name: 'Marika', email: 'marika.pinori@polito.it', password: 'test0123', password_confirmation: 'test0123')
+  end
+
 end
