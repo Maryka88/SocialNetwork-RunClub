742da63bc2f151ea10980f333dea93ef6b42c456
Marika Pinori <marika.pinori@studenti.polito.it>
2014-06-29 12:57:26 +0200

    session helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  include SessionsHelper
+
+  # forzo signout per prevenire attacchi
+  def handle_unverified_request
+    sign_out
+  end
+
 end
