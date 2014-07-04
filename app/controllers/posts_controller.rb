5369c360ae36d47b6a5b073c0df8cc8d86acfc39
Marika Pinori <marika.pinori@studenti.polito.it>
2014-07-04 18:27:42 +0200

    gestione e completamento post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,9 @@ class PostsController < ApplicationController
   # controllo se l'utente è loggato
   before_filter :signed_in_user
 
+  # controllo se l'utente può cancellare il post
+  before_filter :correct_user, only: :destroy
+
   def create
     # creo nuovo post da info contenute in "new post" form
     @post = current_user.posts.build(params[:post])
@@ -11,6 +14,20 @@ class PostsController < ApplicationController
     else
         render 'pages/home'
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to current_user
+  end
+
+  private
+
+  def correct_user
+      # l'utente ha un post con l'id fornito?
+      @post = current_user.posts.find_by_id(params[:id])
+      # se no, redirect a homepage
+      redirect_to root_url if @post.nil?
   end
 
 end
