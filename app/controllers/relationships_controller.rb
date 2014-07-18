91b2f9e5326e2327941e6682da94cbe2fdd92592
Marika Pinori <marika.pinori@studenti.polito.it>
2014-07-18 02:37:34 +0200

    ulteriori modifiche grafiche e funzionalità

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -8,16 +8,18 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    # senza javascript: redirect_to @user
-    respond_with @user
+    # senza javascript
+    redirect_to @user
+    #respond_with @user
     # Rails cerca file create.js.erb per rispondere all'azione con ajax
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    # senza javascript: redirect_to @user
-    respond_with @user
+    # senza javascript
+    redirect_to @user
+    #respond_with @user
     # Rails cerca file destroy.js.erb file er rispondere all'azione con ajax
   end
 end
